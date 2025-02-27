9f6d18ba21a67d45455c28eb3ad61ec1da39c7d6
cyrussamante <cyruss_amante@yahoo.ca>
2025-02-27 18:25:38 -0500

    feat: add footers, change site colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import SiteFooter from "@/components/SiteFooter";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow p-4 bg-gray-850 text-white">
+        <h1>Cyruss Amante</h1>
+        <p>Software Engineering Student @ McMaster University</p>
+        <h2>Experience</h2>
+        <h2>Projects</h2>
+      </main>
+      <SiteFooter />
+    </div>
+  );
 }
---
+++ src/components/SiteFooter.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "flowbite-react";
+
+export function SiteFooter() {
+  return (
+    <Footer container className="bg-gray-950">
+      <div className="w-full text-center">
+        <Footer.Copyright href="#" by="Cyruss Amante" year={2025} />
+      </div>
+    </Footer>
+  );
+}
+
+export default SiteFooter;
